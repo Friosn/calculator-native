cb80629cd805a44dca6dc30d3b1aa1e387014ef4
Friosn <franz.rios.nino@gmail.com>
2023-03-18 11:25:30 +0100

    Project folder structure

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native/types';
+
+function App(): JSX.Element {
+  return <SafeAreaView></SafeAreaView>;
+}
+
+/* const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+}); */
+
+export default App;
